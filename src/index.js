e791fdedc602c80fab1b9f57545d349057652845
alechp <me@alechp.com>
2019-03-14 16:04:04 -0700

    Commented out old code. Issue was reference sectionHeaderUUID in fetchSection instead of sectionUUID

--- src/index.js
+++ src/index.js
@@ -1,22 +1,85 @@
 require("dotenv").config();
+const log = console.log;
 
 const spaceId = process.env.SPACE_ID;
 const apiToken = process.env.API_TOKEN;
 
 const lingo = require("Lingojs");
-console.log(`spaceId: ${spaceId}\napiToken: ${apiToken}`);
-
+log(`spaceId: ${spaceId}\napiToken: ${apiToken}`);
+const capswanKitUuid = "5889C55A-CDD4-48D7-9F6F-28B92635E5A6";
 try {
 	lingo.setup(spaceId, apiToken);
-	console.log(`lingo: ${JSON.stringify(lingo, null, 2)}`);
-	lingo
-		.fetchKits()
-		.then(kits => {
-			console.log(`kits: ${JSON.stringify(kits, null, 2)}`);
+} catch (err) {
+	log(`Lingo module issue ${err}`);
+}
+
+console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+lingo
+	.fetchKit(capswanKitUuid)
+	.then(o => log(`o: ${JSON.stringify(o, null, 2)}`));
+
+console.log("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+function getSectionHeaderUuid(sectionName) {
+	return lingo
+		.fetchKitOutline(capswanKitUuid, 0)
+		.then(sections => {
+			// log(`sections: ${JSON.stringify(sections[0], null, 2)}`);
+			let uuid;
+			log(`sections: ${JSON.stringify(sections, null, 2)}`);
+			sections[0].headers.forEach((v, idx) => {
+				// log(`idx:${idx}\nv:${JSON.stringify(v, null, 2)}`);
+				if (v.name === sectionName) {
+					// console.log(`v.uuid: ${v.uuid}`);
+					uuid = v.uuid;
+				}
+			});
+			return uuid;
 		})
 		.catch(err => {
-			console.log(`fetchKits() ${err}`);
+			log(`getSectionHeaderUuid() ${err}`);
 		});
-} catch (err) {
-	console.log(`lingo setup err: ${err}`);
 }
+
+function getAssetUuids(sectionUuid) {
+	lingo
+		// .fetchSection(sectionUuid, 0)
+		.fetchSection("9533C6B8-599E-4709-9120-9DA8E10A2922", 0)
+		.then(v => {
+			console.log(v);
+			return v;
+		})
+		.catch(err => {
+			log(`getAssetUuids() ${err}`);
+		});
+	log(`sectionUuid: ${sectionUuid}`);
+}
+
+// getSectionHeaderUuid("Icons")
+// 	.then(sectionUuid => {
+// 		console.log(`sectionUuid inside getSectionHeaderUuid: ${sectionUuid}`);
+// 		getAssetUuids(sectionUuid);
+// 		log(`sectionUuid reference: ${sectionUuid}`);
+// 	})
+// 	.catch(err => {
+// 		log(`init ${err}`);
+// 	});
+
+// log(`lingo: ${JSON.stringify(lingo, null, 2)}`);
+
+// lingo
+// 	.fetchKits()
+// 	.then(kits => {
+// 		log(`kits: ${JSON.stringify(kits, null, 2)}`);
+// 	})
+// 	.catch(err => {
+// 		log(`fetchKits() ${err}`);
+// 	});
+
+// lingo
+// 	.fetchKit(capswanKitUuid)
+// 	.then(kit => {
+// 		log(JSON.stringify(kit, null, 2));
+// 	})
+// 	.catch(err => {
+// 		log(`fetchKit() ${err}`);
+// 	});
