e42b1d6460a98c2e95f33ed434e47f46a6b85a83
alechp <me@alechp.com>
2019-03-14 14:01:11 -0700

    Initialized lingo fetchKits call

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 require("dotenv").config();
-console.log(`Hello ${process.env.SAMPLE_ENV}! from node-starter`);
+
+const spaceId = process.env.SPACE_ID;
+const apiToken = process.env.API_TOKEN;
+
+const lingo = require("Lingojs");
+console.log(`spaceId: ${spaceId}\napiToken: ${apiToken}`);
+
+try {
+	lingo.setup(spaceId, apiToken);
+	console.log(`lingo: ${JSON.stringify(lingo, null, 2)}`);
+	lingo
+		.fetchKits()
+		.then(kits => {
+			console.log(`kits: ${kits}`);
+		})
+		.catch(err => {
+			console.log(`fetchKits() ${err}`);
+		});
+} catch (err) {
+	console.log(`lingo setup err: ${err}`);
+}
