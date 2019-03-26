25bf5fe2c84b8728286a08e4d21302043e9282f2
alechp <me@alechp.com>
2019-03-25 18:59:12 -0700

    Udpated TODOs and removed unnecessary comments

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const log = console.log;
 import fs from "fs-extra";
 import lingo from "Lingojs";
 import config from "./index.config";
+
 /**
  * @param {int} spaceId :: Lingo Space ID (6 digits)
  * @param {int} apiToken :: Account root API
  */
+
 export function getLingoSetupVariables(spaceId, apiToken) {
 	if (spaceId == null || apiToken == null) {
 		return [process.env.SPACE_ID, process.env.API_TOKEN];
@@ -19,23 +21,14 @@ export function getLingoSetupVariables(spaceId, apiToken) {
  *
  * @param {string} kitName
  */
-// kitName = "Capswan - Mobile App - Style Guide"
 export async function getKitId(kitName = "Capswan - Mobile App - Style Guide") {
-	//TODO: Add test for accounts which only have single kit
-	//? Not sure whether this is going to be an issue given that the data
-	//? structure would change based on how many kits are returned
 	let kitUuid;
 	try {
 		let kits = await lingo.fetchKits();
-		// log(`kits: ${JSON.stringify(kits, null, 2)}`);
 		kits.forEach(v => {
-			// log(`v:${JSON.stringify(v, null, 2)}`);
 			if (v.name === kitName) {
-				// log(`v.name: ${v.name}`);
-				// log(`v.kit_uuid:${v.kit_uuid}`);
 				kitUuid = v.kit_uuid;
 			}
-			// log(`kitUuid: ${kitUuid}`);
 		});
 		return kitUuid;
 	} catch (err) {
@@ -51,31 +44,18 @@ export async function getRelevantAssetContainers(
 	try {
 		let uuids = { sections: [] };
 		let outline = await lingo.fetchKitOutline(kitId, kitVersion);
-		// log(`outline: ${JSON.stringify(outline, null, 2)}`);
-		// log(`kitId: ${kitId}`);
 		extractTarget.sections.forEach(targetSec => {
-			// log(`targetSec:${JSON.stringify(targetSec, null, 2)}`);
-			//TODO: Rename "originSec" to "outlineSec" for clarity
 			Object.values(outline).forEach(originSec => {
 				//TODO: Add a test for sections with duplicate names
-				// log(`originSec: ${JSON.stringify(originSec, null, 2)}`);
 				let headerUuids = [];
 				if (targetSec.name === originSec.name) {
-					// log(`originSec.uuid: ${originSec.uuid}`);
 					if (targetSec.hasOwnProperty("headers")) {
-						// log(`has headers: ${originSec.name}`);
-						// log(
-						// 	`targetSec headers: ${JSON.stringify(targetSec.headers, null, 2)}`
-						// );
 						//TODO: Add a test for headers with duplicate names
 						targetSec.headers.forEach(tsHeaderName => {
-							// log(`tsHeaderName: ${tsHeaderName}`);
 							originSec.headers.forEach(osHeader => {
 								if (tsHeaderName === osHeader.name) {
-									// log(`${osHeader.name} uuid: ${osHeader.uuid}`);
 									headerUuids.push(osHeader.uuid);
 								}
-								// log(`osHeader: ${JSON.stringify(osHeader, null, 2)}`);
 							});
 						});
 					}
@@ -87,7 +67,6 @@ export async function getRelevantAssetContainers(
 				}
 			});
 		});
-		// log(JSON.stringify(uuids, null, 2));
 		return uuids;
 	} catch (err) {
 		log(`getRelevantAssetContainers() ${err}`);
@@ -96,10 +75,7 @@ export async function getRelevantAssetContainers(
 
 export function formatAssetContainers({ sections } = assetContainers) {
 	let singletonUuids = [];
-	//? Not mapping because assetContainers will always be small.
-	//? Unnecessary loops won't impact performance.
 	sections.forEach((section, idx) => {
-		// log(`section ${idx}: ${JSON.stringify(section, null, 2)}`);
 		if (section.hasOwnProperty("headers") && section.headers.length === 0) {
 			singletonUuids.push(Object.assign({}, { [section.uuid]: null }));
 		} else {
@@ -108,7 +84,6 @@ export function formatAssetContainers({ sections } = assetContainers) {
 			});
 		}
 	});
-	// log(`singleton: ${JSON.stringify(singletonUuids, null, 2)}`);
 	return singletonUuids;
 }
 
@@ -131,15 +106,19 @@ function buildFileName(assetName, assetKeywords) {
 		return assetName;
 	}
 }
-
+/**
+ *
+ * @param {object{<string>:<string>}} singletonUuids
+ * @param {integer} version
+ * @param {integer} page
+ * @param {integer} limit
+ */
 export async function getAssetUuids(
 	singletonUuids,
 	version = 0,
 	page = 1,
 	limit = 2000
 ) {
-	//TODO: Revisit formatAssetContainers and consider consolidating functions
-	//? This feels super clunky.
 	var assetUuids = [];
 	try {
 		for (let s of singletonUuids) {
@@ -207,6 +186,13 @@ export async function getAssetUuids(
 //TODO: Consider checking all file names for duplicates (to prevent unnecessary overwrites)
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
+
+/**
+ *
+ * @param {object} asset
+ * @param {string} outFormat
+ * @param {string} outDir
+ */
 export async function batchDownload(
 	asset,
 	outFormat = "png",
